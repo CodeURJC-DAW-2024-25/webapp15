9de25da9f82e295d54e0cb679b66ac830824f927
Gabim23 <gabigmg23@gmail.com>
2025-02-17 12:12:37 +0100

    Spring Boot

---
+++ stepx/src/main/java/com/stepx/stepx/StepxApplication.java
@@ -0,0 +1,13 @@
+package com.stepx.stepx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepxApplication.class, args);
+	}
+
+}
---
+++ stepx/src/test/java/com/stepx/stepx/StepxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepx.stepx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
