3790e4a5962c366d90648d1732ceb180f94e03fa
D4ng3r25 <silverasebastian82@gmail.com>
2025-02-18 11:32:46 +0100

    changes in shop html

--- stepx/src/main/java/com/stepx/stepx/controller/GeneralController.java
+++ stepx/src/main/java/com/stepx/stepx/controller/GeneralController.java
@@ -5,12 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class ExampleController {
 
-    @GetMapping("/example")
-    public String example(Model model) {
-        model.addAttribute("message", "Welcome to Spring Boot with Mustache!");
+@Controller
+public class GeneralController { //todas las solicitudes "/...." son con el controlador
+
+    @GetMapping("/index")
+    public String showIndex(Model model) {
         return "index";  // nombre de la plantilla Mustache sin la extensión .html
     }
+
+    @GetMapping("/shop")
+    public String showShop(Model model) {
+        return "shop";
+    }
+    
+
 }
