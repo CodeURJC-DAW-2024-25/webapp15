83657c268ca41d28dea68ea930f21017389cee53
D4ng3r25 <silverasebastian82@gmail.com>
2025-02-25 09:52:08 +0100

    data base initialized

--- stepx/src/main/java/com/stepx/stepx/model/Shoe.java
+++ stepx/src/main/java/com/stepx/stepx/model/Shoe.java
@@ -2,6 +2,7 @@ package com.stepx.stepx.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,15 +20,15 @@ public class Shoe {
     // Almacenamos tres imágenes como BLOBs
     @Lob
     @Column(name = "image1")
-    private byte[] image1;
+    private Blob image1;
 
     @Lob
     @Column(name = "image2")
-    private byte[] image2;
+    private Blob image2;
 
     @Lob
     @Column(name = "image3")
-    private byte[] image3;
+    private Blob image3;
 
     @Enumerated(EnumType.STRING)
     private Brand brand;
@@ -72,27 +73,27 @@ public class Shoe {
         this.price = price;
     }
 
-    public byte[] getImage1() {
+    public Blob getImage1() {
         return image1;
     }
 
-    public void setImage1(byte[] image1) {
+    public void setImage1(Blob image1) {
         this.image1 = image1;
     }
 
-    public byte[] getImage2() {
+    public Blob getImage2() {
         return image2;
     }
 
-    public void setImage2(byte[] image2) {
+    public void setImage2(Blob image2) {
         this.image2 = image2;
     }
 
-    public byte[] getImage3() {
+    public Blob getImage3() {
         return image3;
     }
 
-    public void setImage3(byte[] image3) {
+    public void setImage3(Blob image3) {
         this.image3 = image3;
     }
 
--- stepx/src/main/java/com/stepx/stepx/service/DataInitializer.java
+++ stepx/src/main/java/com/stepx/stepx/service/DataInitializer.java
@@ -1,13 +1,20 @@
 package com.stepx.stepx.service;
 
+import javax.sql.rowset.serial.SerialBlob;
+import java.sql.Blob;
 import com.stepx.stepx.model.Shoe;
 import com.stepx.stepx.model.ShoeSizeStock;
 import com.stepx.stepx.repository.ShoeRepository;
 import com.stepx.stepx.repository.ShoeSizeStockRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -30,12 +37,22 @@ public class DataInitializer implements CommandLineRunner {
 
     private void initializeShoesAndStocks() {
         // Create some sample shoes
+
+        //create samples of image
+        Blob image1 = loadImage("/static/images/shopimages/Nike-Air-grey.jpg");
+        Blob image2 = loadImage("/static/images/shopimages/Nike-Air-blue.jpg");
+        Blob image3 = loadImage("/static/images/shopimages/Nike-Air-red.jpg");
+
         Shoe shoe1 = new Shoe();
         shoe1.setName("Air Max 270");
         shoe1.setDescription("Nike Air Max 270 Running Shoes");
         shoe1.setPrice(new BigDecimal("150.00"));
         shoe1.setBrand(Shoe.Brand.NIKE);
         shoe1.setCategory(Shoe.Category.SPORT);
+        shoe1.setImage1(image1);
+        shoe1.setImage2(image2);
+        shoe1.setImage3(image3);
+
 
         Shoe shoe2 = new Shoe();
         shoe2.setName("Stan Smith");
@@ -43,6 +60,9 @@ public class DataInitializer implements CommandLineRunner {
         shoe2.setPrice(new BigDecimal("80.00"));
         shoe2.setBrand(Shoe.Brand.ADIDAS);
         shoe2.setCategory(Shoe.Category.CASUAL);
+        shoe2.setImage1(image1);
+        shoe2.setImage2(image2);
+        shoe2.setImage3(image3);
 
         // Save shoes to the repository
         shoeRepository.save(shoe1);
@@ -53,6 +73,7 @@ public class DataInitializer implements CommandLineRunner {
         stock1.setShoe(shoe1);
         stock1.setSize("42");
         stock1.setStock(10);
+        
 
         ShoeSizeStock stock2 = new ShoeSizeStock();
         stock2.setShoe(shoe1);
@@ -77,5 +98,22 @@ public class DataInitializer implements CommandLineRunner {
         shoeSizeStockRepository.save(stock4);
 
         System.out.println("Sample data initialized successfully!");
+    }
+
+    public Blob loadImage(String imagePath) {
+        try {
+            Resource resource = new ClassPathResource(imagePath);
+            if (!resource.exists()) {
+                System.out.println("Error: No se encontró la imagen en la ruta especificada.");
+                return null;
+            }
+            try (InputStream inputStream = resource.getInputStream()) {
+                byte[] imageBytes = inputStream.readAllBytes();
+                return new SerialBlob(imageBytes);
+            }
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
