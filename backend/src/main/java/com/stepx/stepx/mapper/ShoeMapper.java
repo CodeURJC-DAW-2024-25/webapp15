801e41482c8d4716b0d86df22134ba66baee3bb6
Pearson33 <146853701+Pearson33@users.noreply.github.com>
2025-03-17 12:31:52 +0100

    initialize mappers

---
+++ backend/src/main/java/com/stepx/stepx/mapper/ShoeMapper.java
@@ -0,0 +1,14 @@
+package com.stepx.stepx.mapper;
+
+import com.stepx.stepx.dto.ShoeDTO;
+import com.stepx.stepx.model.Shoe;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel =  "spring")
+public interface ShoeMapper {
+    ShoeMapper INSTANCE = Mappers.getMapper(ShoeMapper.class);
+
+    ShoeDTO toDTO(Shoe shoe);
+    Shoe toEntity(ShoeDTO shoeDTO);
+}
