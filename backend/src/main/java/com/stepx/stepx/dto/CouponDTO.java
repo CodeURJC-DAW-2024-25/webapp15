10ae6e0cffa9ac5d927fae94b7265c87e4d9918f
Gabim23 <gabigmg23@gmail.com>
2025-03-21 17:21:18 +0100

    coupon dto and user dto with records

--- backend/src/main/java/com/stepx/stepx/dto/CouponDTO.java
+++ backend/src/main/java/com/stepx/stepx/dto/CouponDTO.java
@@ -1,57 +1,10 @@
-
 package com.stepx.stepx.dto;
 
 import java.math.BigDecimal;
 
-public class CouponDTO {
-
-    private Long id;
-    private String code;
-    private BigDecimal discount;
-    private Long userId;
-
-    // Constructors
-    public CouponDTO() {
-    }
-
-    public CouponDTO(Long id, String code, BigDecimal discount) {
-        this.id = id;
-        this.code = code;
-        this.discount = discount;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public BigDecimal getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(BigDecimal discount) {
-        this.discount = discount;
-    }
-
-    // toString method
-    @Override
-    public String toString() {
-        return "CouponDTO{" +
-                "id=" + id +
-                ", code='" + code + '\'' +
-                ", discount=" + discount +
-                '}';
-    }
+public record CouponDTO(
+    Long id,
+    String code,
+    BigDecimal discount
+) {
 }
--- backend/src/main/java/com/stepx/stepx/dto/UserDTO.java
+++ backend/src/main/java/com/stepx/stepx/dto/UserDTO.java
@@ -3,112 +3,14 @@ package com.stepx.stepx.dto;
 import java.sql.Blob;
 import java.util.List;
 
-
-public class UserDTO {
-
-    private Long id;
-    private boolean imageBool;
-    private Blob imageUser;
-    private String firstname;
-    private String lastName;
-    private List<String> roles;
-    private String username;
-    private String email;
-    private List<OrderShoesDTO> orders;
-    private List<ReviewDTO> reviews;
-
-    // Constructors
-    public UserDTO() {
-    }
-
-    public UserDTO(Long id, Blob imageUser, String firstname, String lastName, List<String> roles, String username, String email, List<OrderShoesDTO> orders) {
-        this.id = id;
-        this.imageUser = imageUser;
-        this.firstname = firstname;
-        this.lastName = lastName;
-        this.roles = roles;
-        this.username = username;
-        this.email = email;
-        this.orders = orders;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Blob getImageUser() {
-        return imageUser;
-    }
-
-    public void setImageUser(Blob imageUser) {
-        this.imageUser = imageUser;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<OrderShoesDTO> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<OrderShoesDTO> orders) {
-        this.orders = orders;
-    }
-
-    // toString method
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "id=" + id +
-                ", imageUser=" + imageUser +
-                ", firstname='" + firstname + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", roles=" + roles +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", orders=" + orders +
-                '}';
-    }
+public record UserDTO(
+    Long id,
+    Blob imageUser,
+    String firstname,
+    String lastName,
+    List<String> roles,
+    String username,
+    String email,
+    List<OrderShoesDTO> orders
+) {
 }
