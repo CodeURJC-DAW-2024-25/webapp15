3577c08e8e1fba0e82c33c73e4d6e57e67e99baf
ElineeF <en.freites.2022@alumnos.urjc.es>
2025-03-24 22:57:46 +0100

    Security Implements

--- backend/src/main/java/com/stepx/stepx/security/WebSecurityConfig.java
+++ backend/src/main/java/com/stepx/stepx/security/WebSecurityConfig.java
@@ -1,17 +1,26 @@
 package com.stepx.stepx.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.stepx.stepx.controller.web.CustomAuthenticationSuccessHandler;
+import com.stepx.stepx.security.jwt.JwtRequestFilter;
+import com.stepx.stepx.security.jwt.UnauthorizedHandlerJwt;
+
+import aj.org.objectweb.asm.commons.Method;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +28,13 @@ public class WebSecurityConfig {
 
 	// @Autowired
 	private RepositoryUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
+
 
 	public WebSecurityConfig(RepositoryUserDetailsService userDetailsService) {
 		this.userDetailsService = userDetailsService;
@@ -45,14 +61,54 @@ public class WebSecurityConfig {
 	}
 
 	@Bean
+	@Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authenticationProvider(authenticationProvider());
+		
+		http
+			.securityMatcher("/api/**")
+			.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+		
+		http
+			.authorizeHttpRequests(authorize -> authorize
+                    // PRIVATE ENDPOINTS
+						//users
+                    .requestMatchers(HttpMethod.POST,"/api/OrderItem/**", "/api/OrderShoes/**","/api/reviews/**","/api/ShoeSizeStock/**", "/api/user/**").hasRole("USER")
+                    .requestMatchers(HttpMethod.PUT,"/api/OrderItem/**", "/api/reviews/**", "/api/ShoeSizeStock/**").hasRole("USER")
+					.requestMatchers(HttpMethod.GET,"/api/OrderItem/**","/api/OrderShoes/**").hasRole("USER")
+					.requestMatchers(HttpMethod.DELETE,"/api/admin/**", "/api/OrderItem/**", "/api/reviews/**", "/api/user/**").hasRole("USER")
+						//Admins
+                    .requestMatchers(HttpMethod.DELETE,"/api/admin/**","/api/reviews/**", "/api/Shoe/**", "/api/user/**").hasRole("ADMIN")
+					.requestMatchers(HttpMethod.GET,"/api/admin/**", "/api/reviews/**","/api/Shoe/**", "/api/user/**").hasRole("ADMIN")
+					.requestMatchers(HttpMethod.PUT,"/api/admin/**","/api/ShoeSizeStock/**").hasRole("ADMIN")
+					.requestMatchers(HttpMethod.POST,"/api/admin/**", "/api/Shoe/**","/api/ShoeSizeStock/**").hasRole("ADMIN")
+					// PUBLIC ENDPOINTS
+					.requestMatchers("/api/shoe", "/**/image/**", "/api/Shoe/**").permitAll()
+					.anyRequest().permitAll()
+			);
+		
+        http.formLogin(formLogin -> formLogin.disable());
+
+        http.csrf(csrf -> csrf.disable());
+
+        http.httpBasic(httpBasic -> httpBasic.disable());
+
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
 	public SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationSuccessHandler successHandler)
 			throws Exception {
 
 		http.authenticationProvider(authenticationProvider());
-		http .csrf(csrf -> csrf
-				.ignoringRequestMatchers("/api/**") // ✅ Ignorar CSRF solo en la API
-				)
-				.authorizeHttpRequests(authorize -> authorize
+
+		http .authorizeHttpRequests(authorize -> authorize
 						// PUBLIC PAGES
 						.requestMatchers("/index", "/register-user", "/shop/**", "/partials/**", "/createAccount",
 								"/errorPage", "/api/**", "/api/create-product")
