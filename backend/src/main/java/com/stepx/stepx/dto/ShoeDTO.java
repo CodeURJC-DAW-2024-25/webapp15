a1958aaffbb23b935d78f0a9143979da6c1e358e
Gabim23 <gabigmg23@gmail.com>
2025-03-25 02:38:45 +0100

    edit-product fixed with dtos

--- backend/src/main/java/com/stepx/stepx/dto/ShoeDTO.java
+++ backend/src/main/java/com/stepx/stepx/dto/ShoeDTO.java
@@ -6,7 +6,7 @@ import java.util.List;
 public record ShoeDTO(
     Long id,
     String name,
-    String shortDescription,
+    String shortdescription,
     String longDescription,
     BigDecimal price,
     String brand,
