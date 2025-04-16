975609a74c18fdf3603ce08601fa2a80f3c70b27
Gabim23 <gabigmg23@gmail.com>
2025-04-16 11:30:12 +0200

    footer and implementing the css

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     HttpClientModule,
