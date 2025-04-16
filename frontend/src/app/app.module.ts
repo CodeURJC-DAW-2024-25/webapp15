b67c4597557cf2cf9bdca010aa23d76d31808541
Gabim23 <gabigmg23@gmail.com>
2025-04-16 12:05:22 +0200

    coupon template, still not implemented

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
+import { CouponComponent } from './components/coupon/coupon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    CouponComponent
   ],
   imports: [
     HttpClientModule,
