1902c631db1ff788e80da3551d9e2c22115a173b
Pearson33 <146853701+Pearson33@users.noreply.github.com>
2025-04-23 12:01:50 +0200

    Reccomended products and reqis loggued in loginservice

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of } from 'rxjs';
+import {UserDTO} from "../dtos/user.dto";
 
 
 interface AuthResponse {
@@ -12,6 +13,8 @@ interface AuthResponse {
 @Injectable({ providedIn: 'root' })
 export class LoginService {
   private readonly API_URL = "/api/v1";
+  public user: UserDTO | null = null;
+  public logged: boolean = false;
 
   constructor(private http: HttpClient) {}
 
@@ -24,5 +27,23 @@ export class LoginService {
         headers: { 'Content-Type': 'application/json' }
       }
     );
+  }
+
+  public reqIsLogged() {
+    this.http.get<UserDTO>(`${this.API_URL}/user/me`, { withCredentials: true })
+      .subscribe(
+        (response) => {
+          console.log("User logged: " + JSON.stringify(response));
+          this.user = response;
+          this.logged = true;
+        },
+        (error) => {
+          this.user = null;
+          this.logged = false;
+          if (error.status !== 404) {
+            console.error("Error when asking if logged: " + JSON.stringify(error));
+          }
+        }
+      );
   }
 }
