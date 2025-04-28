1529989568172fcda0f859dbd34539b984104178
ElineeF <en.freites.2022@alumnos.urjc.es>
2025-04-28 11:25:10 +0200

    Fixing routes in module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginModalComponent } from './components/login-modal/login-modal.component';
 import { ShopComponent } from './components/shop/shop.component';
 import { AdminComponent } from './components/admin/admin.component';
-import { ProfileDataComponent } from './components/profile-data/profile-data.component';
 import { ShoeEditComponent } from './components/shoe-edit/shoe-edit.component';
 import { CreateShoeComponent } from './components/create-shoe/create-shoe.component';
 import { ShoeInfoComponent } from './components/singleProduct/shoeInfo.component';
-import { OrderHistoryComponent } from './components/order-history/order-history.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CheckoutComponent } from './components/CheckOut/checkout.component';
@@ -17,10 +15,8 @@ const routes: Routes = [
   {path:'shop',component:ShopComponent},
   {path:'shop/edit/:id', component:ShoeEditComponent},
   { path: 'admin', component: AdminComponent },
-  { path: 'profileData/:id', component: ProfileDataComponent },
   { path: 'create-shoe', component: CreateShoeComponent },
   { path: 'shop/single-product/:id', component: ShoeInfoComponent},
-  { path: 'orderHistory/:id', component: OrderHistoryComponent },
   { path: 'profilePage/:id', component: ProfilePageComponent },
   { path: 'register', component: RegisterComponent },
   {path: 'checkout', component: CheckoutComponent},
