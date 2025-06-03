ec391f229f5aca149eb630ba8a2c74f52f79bbd2
Pearson33 <146853701+Pearson33@users.noreply.github.com>
2025-06-03 19:22:01 +0200

    load more reviews and submit review

---
+++ frontend/src/app/services/reviews.service.ts
@@ -0,0 +1,29 @@
+// src/app/services/reviews.service.ts
+import { HttpClient,HttpParams } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ReviewDTO } from '../dtos/review.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+  private baseUrl = '/api/v1/reviews'; // ajusta si tu puerto o ruta base es distinta
+
+  constructor(private http: HttpClient) {}
+
+  getReviewsByShoeId(shoeId: number, page:number, size:number = 2): Observable<ReviewDTO[]> {
+    console.log(`Getting reviews for shoe ID: `+ this.http.get<ReviewDTO[]>(`${this.baseUrl}/Shoe/${shoeId}`));
+    const params =  new HttpParams().set('page',page).set('size',size);
+    return this.http.get<ReviewDTO[]>(`${this.baseUrl}/paginated/${shoeId}`, {params});
+  }
+  submitReview(review: ReviewDTO) {
+  return this.http.post<ReviewDTO>(`${this.baseUrl}`, review);
+}
+
+
+  // Si en el futuro quieres añadir más funciones:
+  // createReview(dto: ReviewDTO): Observable<ReviewDTO> { ... }
+  // deleteReview(id: number): Observable<ReviewDTO> { ... }
+  // updateReview(id: number, dto: ReviewDTO): Observable<ReviewDTO> { ... }
+}
